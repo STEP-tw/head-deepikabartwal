c34b18dece626ecbc76cc92dbc1a973dfeb649ad
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-11 01:10:15 +0530

    modified head and renamed validate count to invalid count

--- src/headLib.js
+++ src/headLib.js
@@ -16,30 +16,33 @@ const parseArgs = function(args){
   return createArgsObject("-n","10",args);
 }
 
-const delimiter = {
-  'lines':'\n',
-  'characters':''
+const separator= {
+  'line':'\n',
+  'byte':''
 }
 
-const validateCount = function(countArg){
-  return countArg > 0 && !isNaN(countArg) 
+const invalidCount = function(countArg){
+  return isNaN(countArg - 0)||countArg<1;
 }
 
 const head = function(fs,args){
   let {option,count,filenames} = parseArgs(args);
-  let delim = delimiter[option];
-  if(!validateCount(count)){
-    return "head: illegal line count -- "+count;
+  let delim = separator[option];
+  if(invalidCount(count)){
+    return "head: illegal "+option+" count -- "+count;
   }
   const getContent = function(path){
     if(!fs.existsSync(path)){
       return generateErrorText("nf"+path);
     }
     let lines = fs.readFileSync(path,'utf-8').split(delim);
-    return take(lines,count).join(delim);
+    return take(lines,+count).join(delim);
   }
   const getContentWithHeadings = function(path){
     let heading = "==> "+path+" <==";
+    if(!fs.existsSync(path)){
+      return generateErrorText("nf"+path);
+    }
     return heading + "\n" + getContent(path);node
   }
   if(filenames.length>1){
@@ -49,9 +52,9 @@ const head = function(fs,args){
 }
 
 const createArgsObject = function(type,count,filenames){
-  let option = 'lines';
+  let option = 'line';
   if(type == '-c'){
-    option = 'characters';
+    option = 'byte';
   }
   return {option,count,filenames};
 }
@@ -62,12 +65,12 @@ const validateOption = function(optionParaCandidate){
 
 const parseArgsWithOption = function(args){
   if(validateOption(args[0])){
-    return createArgsObject(args[0],+args[1],args.slice(2));
+    return createArgsObject(args[0],args[1],args.slice(2));
   }
   if(!isNaN(args[0].slice(1))){
-      return createArgsObject("-n",Math.abs(args[0]),args.slice(1));
+      return createArgsObject("-n",args[0].slice(1),args.slice(1));
   }
-  return createArgsObject(args[0].slice(0,2),+args[0].slice(2),args.slice(1));
+  return createArgsObject(args[0].slice(0,2),args[0].slice(2),args.slice(1));
 }
 
 module.exports = {
