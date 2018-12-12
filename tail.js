875a50cd79f352d017cc63c61cad047a388368d7
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-12 11:11:32 +0530

    modified tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,3 @@
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
 const tail = require("./src/headLib.js").tail;
 const fs = require("fs");
 process.stdout.write(tail(fs,process.argv.slice(2)));
