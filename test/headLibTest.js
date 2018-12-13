7d7047542780e92e2b87a3d34d100f4f860e9cdd
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-13 10:05:39 +0530

    added more tail level for multiple files

--- test/headLibTest.js
+++ test/headLibTest.js
@@ -412,4 +412,10 @@ describe('tail',function(){
       equal(tail(fs,argv('-na fiveLines.txt fiveLines.txt')),expected_output);
     });
   });
+  describe('node tail -n a fiveLines.txt fiveLines.txt',function(){
+    it('should return error for illegal offset',function(){
+      let expected_output = 'tail: illegal offset -- a';
+      equal(tail(fs,argv('-n a fiveLines.txt fiveLines.txt')),expected_output);
+    });
+  });
 });
