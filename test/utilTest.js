b7b59e3c8730634cd7750c7990e4b492b30e6a24
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-17 16:52:57 +0530

    added last test and modified take's tests

--- test/utilTest.js
+++ test/utilTest.js
@@ -1,16 +1,33 @@
-const take = require("../src/util.js").take;
+const { take, last } = require("../src/util.js");
 
 const { deepEqual, equal } = require("assert");
 
 describe("take", function() {
-  it("should return number of lines specified in argument", function() {
-    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let expected_output = [1, 2, 3];
-    deepEqual(take(input, 3), expected_output);
+  it("should return number of lines specified in argument from beginning", function() {
+    let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let expectedOutput = [1, 2, 3];
+    let actual = take(numberList, 3);
+    deepEqual(actual, expectedOutput);
   });
   it("should return same number of lines as input when number of lines specified is more than lines in input", function() {
-    let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     let expectedOutput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    deepEqual(take(input, 30), expectedOutput);
+    let actual = take(numberList, 30);
+    deepEqual(actual, expectedOutput);
+  });
+});
+
+describe("last", function() {
+  it("should return number of lines specified in argument from end", function() {
+    let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let expectedOutput = [6, 7, 8, 9, 10];
+    let actual = last(numberList, 5);
+    deepEqual(actual, expectedOutput);
+  });
+  it("should return as many lines as files contain when count is larger than the number of files", function() {
+    let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let expectedOutput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let actual = last(numberList, 15);
+    deepEqual(actual, expectedOutput);
   });
 });
