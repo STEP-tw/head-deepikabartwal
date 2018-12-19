068119b4169ef3bf13bb9869640ea2e2fcf325a4
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-19 10:45:21 +0530

    removed global object and formatted file

--- src/headAndTail.js
+++ src/headAndTail.js
@@ -1,15 +1,10 @@
-const take = require("./util.js").take;
+const take = require('./util/array.js').take;
 
 const parseArgs = function(args) {
-  if (args[0].startsWith("-")) {
+  if (args[0].startsWith('-')) {
     return parseArgsWithOption(args);
   }
-  return createArgsObject("-n", "10", args);
-};
-
-const separator = {
-  line: "\n",
-  byte: ""
+  return createArgsObject('-n', '10', args);
 };
 
 const isInvalidCount = function(countArg) {
@@ -17,41 +12,42 @@ const isInvalidCount = function(countArg) {
 };
 
 const head = function(args, fs) {
-  let { option, count, filenames } = parseArgs(args);
-  let delim = separator[option];
+  let { option, delim, count, filenames } = parseArgs(args);
   if (isInvalidCount(count)) {
-    return "head: illegal " + option + " count -- " + count;
+    return 'head: illegal ' + option + ' count -- ' + count;
   }
   const getContent = function(path) {
     if (!fs.existsSync(path)) {
-      return "head: " + path + ": No such file or directory";
+      return 'head: ' + path + ': No such file or directory';
     }
-    let lines = fs.readFileSync(path, "utf-8").split(delim);
+    let lines = fs.readFileSync(path, 'utf-8').split(delim);
     return take(lines, +count).join(delim);
   };
   const getContentWithHeadings = function(path) {
-    let heading = "==> " + path + " <==";
+    let heading = '==> ' + path + ' <==';
     if (!fs.existsSync(path)) {
-      return "head: " + path + ": No such file or directory";
+      return 'head: ' + path + ': No such file or directory';
     }
-    return heading + "\n" + getContent(path);
+    return heading + '\n' + getContent(path);
   };
   if (filenames.length > 1) {
-    return filenames.map(getContentWithHeadings).join("\n");
+    return filenames.map(getContentWithHeadings).join('\n');
   }
   return filenames.map(getContent).join(delim);
 };
 
 const createArgsObject = function(type, count, filenames) {
-  let option = "line";
-  if (type == "-c") {
-    option = "byte";
+  let option = 'line';
+  let delim = '\n';
+  if (type == '-c') {
+    option = 'byte';
+    delim = '';
   }
-  return { option, count, filenames };
+  return { option, count, delim, filenames };
 };
 
 const isValidOption = function(optionCandidate) {
-  return optionCandidate == "-n" || optionCandidate == "-c";
+  return optionCandidate === '-n' || optionCandidate === '-c';
 };
 
 const parseArgsWithOption = function(args) {
@@ -59,23 +55,22 @@ const parseArgsWithOption = function(args) {
     return createArgsObject(args[0], args[1], args.slice(2));
   }
   if (!isNaN(args[0].slice(1))) {
-    return createArgsObject("-n", args[0].slice(1), args.slice(1));
+    return createArgsObject('-n', args[0].slice(1), args.slice(1));
   }
   return createArgsObject(args[0].slice(0, 2), args[0].slice(2), args.slice(1));
 };
 
 const tail = function(args, fs) {
-  let { option, count, filenames } = parseArgs(args);
-  let delim = separator[option];
+  let { option, count, delim, filenames } = parseArgs(args);
   if (isNaN(count)) {
-    return "tail: illegal offset -- " + count;
+    return 'tail: illegal offset -- ' + count;
   }
   const getContent = function(path) {
     if (!fs.existsSync(path)) {
-      return "tail: " + path + ": No such file or directory";
+      return 'tail: ' + path + ': No such file or directory';
     }
     let lines = fs
-      .readFileSync(path, "utf-8")
+      .readFileSync(path, 'utf-8')
       .split(delim)
       .reverse();
     return take(lines, Math.abs(count))
@@ -83,14 +78,14 @@ const tail = function(args, fs) {
       .join(delim);
   };
   const getContentWithHeadings = function(path) {
-    let heading = "==> " + path + " <==";
+    let heading = '==> ' + path + ' <==';
     if (!fs.existsSync(path)) {
-      return "tail: " + path + ": No such file or directory";
+      return 'tail: ' + path + ': No such file or directory';
     }
-    return heading + "\n" + getContent(path);
+    return heading + '\n' + getContent(path);
   };
   if (filenames.length > 1) {
-    return filenames.map(getContentWithHeadings).join("\n");
+    return filenames.map(getContentWithHeadings).join('\n');
   }
   return filenames
     .map(getContent)
